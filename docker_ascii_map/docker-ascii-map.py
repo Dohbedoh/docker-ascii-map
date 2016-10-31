1767911913e7a4eb4c56eff52cd4ba20adfa7d86
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2016-10-31 14:20:00 +0100

    Add terminal detection and tests.

--- docker_ascii_map/docker-ascii-map.py
+++ docker_ascii_map/docker-ascii-map.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
-import argparse
 import sys
-from docker_ascii_map import __version__
 
+import params
+from docker_ascii_map.ascii_render import Renderer
 from docker_ascii_map.docker_config import ConfigParser
 
-from docker_ascii_map.ascii_render import Renderer
-
-parser = argparse.ArgumentParser(description='Display the docker host contents on a visual map.')
-parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
-parser.add_argument('-c', '--color', action='store_const', const='color', help='Use color in map display')
-parser.add_argument('-m', '--mono', action='store_const', const='mono', help='Render the map in monochrome')
-
 if __name__ == '__main__':
-    args = parser.parse_args()
-    color_mode = args.color and not args.mono
-
+    color_mode = params.get_input_parameters()[0]
     config_parser = ConfigParser()
     renderer = Renderer()
 
---
+++ tests/params_test.py
@@ -0,0 +1,29 @@
+import unittest
+from argparse import ArgumentParser
+from unittest.mock import patch
+
+import params
+
+
+class Args:
+    def __init__(self, mono=None, color=None):
+        self.mono = mono
+        self.color = color
+
+
+class ParametersTests(unittest.TestCase):
+    def test_monoterm(self):
+        with patch('os.getenv', return_value='vt100') as mock_getenv:
+            with patch.object(ArgumentParser, 'parse_args', return_value=Args()) as mock_parse:
+                r = params.get_input_parameters()
+                self.assertEqual(False, r[0])
+
+    def test_colorterm(self):
+        with patch('os.getenv', return_value='xterm-256color') as mock_getenv:
+            with patch.object(ArgumentParser, 'parse_args', return_value=Args()) as mock_parse:
+                r = params.get_input_parameters()
+                self.assertEqual(True, r[0])
+
+
+if __name__ == '__main__':
+    unittest.main()
